bd89c96fce5365e9d256515978336dcf88b4cf93
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-02-08 01:23:33 +1100

    Fixed util.random.randomBase64 not using its length parameter

--- source/util/random.d
+++ source/util/random.d
@@ -6,7 +6,7 @@ import std.random;
 import std.algorithm;
 
 string randomBase64(size_t length) {
-    auto bytes = rndGen().map!(a => cast(ubyte)a).take(32);
+    auto bytes = rndGen().map!(a => cast(ubyte)a).take(length);
 
     return Base64.encode(bytes);
 }
