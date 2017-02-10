ff6be5605e4208ed46a96957f97f03379328aaaf
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-02-10 23:41:47 +1100

    Fixed random not being seeded properly

--- source/util/random.d
+++ source/util/random.d
@@ -6,7 +6,8 @@ import std.random;
 import std.algorithm;
 
 string randomBase64(size_t length) {
-    auto bytes = rndGen().map!(a => cast(ubyte)a).take(length);
+    rndGen.seed(unpredictableSeed);
+    auto bytes = rndGen.map!(a => cast(ubyte)a).take(length);
 
-    return Base64.encode(bytes);
+    return Base64URL.encode(bytes);
 }
