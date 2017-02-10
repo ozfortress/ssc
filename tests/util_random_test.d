cab78bf7ded69f4345aa9fca5264a07eea3f2341
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-02-10 23:48:29 +1100

    Added tests for util.random

---
+++ tests/util_random_test.d
@@ -0,0 +1,25 @@
+module util_random_test;
+import unit_threaded;
+
+import std.algorithm;
+
+import util.random;
+
+@("Uniqueness") @system unittest {
+    auto strs = [
+        randomBase64(12),
+        randomBase64(12),
+        randomBase64(12),
+        randomBase64(12),
+        randomBase64(12),
+        randomBase64(12),
+        randomBase64(12),
+        randomBase64(12),
+        randomBase64(12),
+    ];
+
+    foreach (str; strs) {
+        auto count = strs.count(str);
+        count.shouldEqual(1);
+    }
+}
