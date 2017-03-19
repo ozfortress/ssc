450398f774965bc29884cefa5c28b8fb4dc210b6
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-03-19 11:12:48 +1100

    Added heavy as production server and moved staging server to staging config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+server "40.126.229.205", :user => "deploy", :roles => %{app}
 
 # role-based syntax
 # ==================
