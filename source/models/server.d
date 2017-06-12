613c89b9984f0c5ad07031f37b8bcbdbe842e940
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-06-12 15:02:01 +1000

    Fixed servers restarting before config is updated in memory. Fixes #8.

--- source/models/server.d
+++ source/models/server.d
@@ -375,11 +375,6 @@ class Server {
      * Use to update settings on a running server by setting the dirty flag and waiting for a time to restart
      */
     private void reload(Server config) {
-        // Make dirty if server options change
-        if (executable != config.executable || options != config.options) {
-            makeDirty();
-        }
-
         synchronized (this) {
             executable = config.executable;
             options    = config.options;
@@ -393,6 +388,11 @@ class Server {
             bookingStartCommand = config.bookingStartCommand;
             bookingEndCommand   = config.bookingEndCommand;
             logPath             = config.logPath;
+        }
+
+        // Make dirty if server options change
+        if (executable != config.executable || options != config.options) {
+            makeDirty();
         }
     }
 
